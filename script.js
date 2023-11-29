0d8fcd5082ab073972c0ef7e16a0ada32054dfa8
M. Ibaneza Fendera A. <149344118+KyozuID@users.noreply.github.com>
2023-11-29 22:42:19 +0700

    Menambahkan efek scroll

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ window.onscroll = () => {
     menu.classList.remove('bx-x')
     navlist.classList.remove('open')
 }
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration: '2500',
+    reset: true
+})
+
+sr.reveal ('.home-text',{delay:300})
+sr.reveal ('.home-img',{delay:400})
+sr.reveal ('.container',{delay:400})
+
+sr.reveal ('.about-img',{})
+sr.reveal ('.about-text',{delay:300})
+
+sr.reveal ('.middle-text',{})
+sr.reveal ('.row-btn,.shop-content',{delay:300})
+
+sr.reveal ('.riview-content,.contact',{delay:300})
