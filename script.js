3516c543ccc58ee8654958a75242d06a303ca32b
M. Ibaneza Fendera A. <149344118+KyozuID@users.noreply.github.com>
2023-11-30 19:30:13 +0700

    Merapikan codingan

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Buat Navbar
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', function(){
     header.classList.toggle('sticky', window.scrollY > 80)
 })
 
+
+
+// Buat responsif
 let menu = document.querySelector('#menu-icons')
 let navlist = document.querySelector('.navlist')
 
